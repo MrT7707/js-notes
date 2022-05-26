33a5a8f708307b3a99e710f437eb93b10aae6d03
tan_x <765331651@qq.com>
2022-05-26 11:50:21 +0800

    fix： 修改目录结构

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -33,14 +33,14 @@ const sidebar = buildChildren(workPath);
 
 
 module.exports = {
-  title: "前端随笔 FE-Essay",
+  title: "个人笔记",
   description:
-    "记录前端重要知识点和遇到的好文章，同时还有前端重要算法知识，但最关键的是包含各大小厂真题。",
+    "记录个人学习路上的前端重要知识点和遇到的好文章",
   themeConfig: {
-    nav: [{ text: "GitHub", link: "https://github.com/i-want-offer" }],
+    nav: [{ text: "GitHub", link: "https://github.com/MrT7707" }],
     sidebar,
   },
   dest: path.resolve(__dirname, "../", "../", "dist"),
-  base: "/FE-Essay/",
+  base: "/js-notes/",
   evergreen: true,
 };
